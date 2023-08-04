899bb78454811c32827e6f93a58e4282e4b48dea
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-08-04 18:03:04 +0600

    Project Setup done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+};
